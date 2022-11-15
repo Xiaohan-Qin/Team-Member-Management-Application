7cfd916a51d5859f5061b5615fee539c6d6254f1
qinxhannah <qin.xia@northeastern.edu>
2022-11-15 00:48:18 -0800

    added mysql query file

---
+++ mysql_query.sql
@@ -0,0 +1,21 @@
+CREATE SCHEMA IF NOT EXISTS TeamMemberManagementApplication;
+USE TeamMemberManagementApplication;
+ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY 'password';
+
+DROP TABLE IF EXISTS TeamMembers;
+
+CREATE TABLE TeamMembers (
+	memberId INT AUTO_INCREMENT,
+	firstName VARCHAR(255) NOT NULL,
+	lastName VARCHAR(255) NOT NULL,
+	phone  VARCHAR(255) NOT NULL,
+    email  VARCHAR(255) NOT NULL,
+    role ENUM("admin", "regular"),
+	CONSTRAINT pk_TeamMembers_memberId PRIMARY KEY (MemberId)
+);
+
+INSERT INTO TeamMembers (firstName, lastName, Phone, email, role) 
+VALUES("Peter", "Dong", "206-443-7080", "peterD@gmail.com", "regular");
+
+INSERT INTO TeamMembers (firstName, lastName, phone, email, role) 
+VALUES("Hazel", "Lee", "206-579-8804", "HazelL@gmail.com", "admin");
